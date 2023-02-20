f0b1a7534f22deaf5415a4668eaa48cd5b66d619
MasterHaku <girardhoel@gmail.com>
2023-02-20 21:40:06 +0100

    Ajout HTTPCodes.php dans Utils

---
+++ app/Utils/HTTPCodes.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Utils;
+
+class HTTPCodes {
+    const OK = 0;
+    const NO_CONTENT = 204;
+    const BAD_REQUEST = 400;
+    const UNAUTHORIZED = 401;
+    const FORBIDDEN = 403;
+    const NOTFOUND = 404;
+}
